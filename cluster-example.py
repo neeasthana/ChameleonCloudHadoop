8d749163be9590a0b8545d63a49c21e7195819b5
Neeraj <neeasthana@gmail.com>
2015-09-04 15:21:58 -0500

    simple script to access spark cluster

---
+++ cluster-example.py
@@ -0,0 +1,34 @@
+#Neeraj Asthana for LCDM group under Professor Robert Brunner
+#University of Illinois at Urbana-Champaign
+#Accessing PySpark Cluster from IPython notebook
+
+#Initializes a proper Spark Context(sc)
+#Will be included inside of a startup script so it will be not viewable to user
+import os, sys
+
+spark_home = os.environ.get('SPARK_HOME', None)
+sys.path.insert(0, spark_home + "/python")
+
+sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.8.2.1-src.zip'))
+
+import pyspark
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setMaster("spark://10.0.3.70:7077").setAppName("Testing Neeraj")
+sc = SparkContext(conf=conf)
+
+
+#Ensure that there is an existing Spark context
+sc
+
+#Load text file from HDFS storage
+text_file = sc.textFile('hdfs://10.0.3.113:9000/home/ubuntu/data/1987.csv')
+
+text_file.take(10)
+
+#Counting the number of elements in 1987.csv
+text_file.count()
+
+#Close the Spark Context to unallocate the resources
+sc.stop()
+sc
